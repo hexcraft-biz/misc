003b382cbba711f857b7e0b987bbe5f71c38070e
Boss Yu <cure1218@gmail.com>
2022-06-30 07:05:52 +0000

    boss

--- xuuid/core.go
+++ xuuid/core.go
@@ -1,24 +1,35 @@
 package xuuid
 
 import (
+	"database/sql/driver"
 	"encoding/json"
 	"github.com/google/uuid"
 )
 
 type UUID uuid.UUID
 
-func (fu *UUID) UnmarshalJSON(data []byte) error {
+func (xuuid *UUID) UnmarshalJSON(data []byte) error {
 	var s string
 	if err := json.Unmarshal(data[:], &s); err != nil {
 		return err
 	} else if u, err := uuid.Parse(s); err != nil {
 		return err
 	} else {
-		*fu = UUID(u)
+		*xuuid = UUID(u)
 		return nil
 	}
 }
 
-func (fu UUID) MarshalJSON() ([]byte, error) {
-	return []byte(uuid.UUID(fu).String()), nil
+func (xuuid UUID) MarshalJSON() ([]byte, error) {
+	return []byte(uuid.UUID(xuuid).String()), nil
+}
+
+func (xuuid *UUID) Scan(src interface{}) error {
+	u := (*uuid.UUID)(xuuid)
+	return u.Scan(src)
+}
+
+func (xuuid UUID) Value() (driver.Value, error) {
+	u := uuid.UUID(xuuid)
+	return u.Value()
 }
