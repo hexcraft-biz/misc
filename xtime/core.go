683e8c7d6dd9f1074746009e4544177d30d642d2
Boss Yu <cure1218@gmail.com>
2022-07-12 14:43:25 +0000

    boss

--- xtime/core.go
+++ xtime/core.go
@@ -1,11 +1,16 @@
 package xtime
 
 import (
+	"database/sql"
+	"database/sql/driver"
 	"encoding/json"
 	"fmt"
 	"time"
 )
 
+//================================================================
+//
+//================================================================
 type TimeRFC3339 time.Time
 
 func (ft *TimeRFC3339) UnmarshalJSON(data []byte) error {
@@ -22,4 +27,38 @@ func (ft *TimeRFC3339) UnmarshalJSON(data []byte) error {
 
 func (ft TimeRFC3339) MarshalJSON() ([]byte, error) {
 	return []byte(fmt.Sprintf("\"%s\"", time.Time(ft).UTC().Truncate(time.Second).Format(time.RFC3339))), nil
+}
+
+//================================================================
+//
+//================================================================
+type NullTimeRFC3339 sql.NullTime
+
+func (ntf *NullTimeRFC3339) UnmarshalJSON(data []byte) error {
+	var s string
+	if err := json.Unmarshal(data[:], &s); err != nil {
+		return err
+	} else if t, err := time.ParseInLocation(time.RFC3339, s, time.UTC); err != nil {
+		return err
+	} else {
+		*ntf = NullTimeRFC3339(sql.NullTime{Valid: true, Time: t})
+		return nil
+	}
+}
+
+func (ntf NullTimeRFC3339) MarshalJSON() ([]byte, error) {
+	nt := sql.NullTime(ntf)
+	if nt.Valid {
+		return []byte(fmt.Sprintf("\"%s\"", nt.Time.UTC().Truncate(time.Second).Format(time.RFC3339))), nil
+	} else {
+		return nil, nil
+	}
+}
+
+func (ntf NullTimeRFC3339) Value() (driver.Value, error) {
+	return sql.NullTime(ntf).Value()
+}
+
+func (ntf *NullTimeRFC3339) Scan(value interface{}) error {
+	return (*sql.NullTime)(ntf).Scan(value)
 }
