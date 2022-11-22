dba06dbfdc53ba06af9a2ba6f1e176479b6981ae
Boss Yu <cure1218@gmail.com>
2022-11-22 23:44:24 +0000

    boss

--- xtime/core.go
+++ xtime/core.go
@@ -9,51 +9,48 @@ import (
 //================================================================
 // Time
 //================================================================
-type Time struct {
-	time.Time
-}
+type Time time.Time
 
 func NowUTC() Time {
-	t := time.Now().UTC()
-	return Time{Time: t}
+	return Time(time.Now().UTC())
 }
 
 func MysqlMin() Time {
-	return Time{
-		Time: time.Date(1000, time.January, 1, 0, 0, 0, 0, time.UTC),
-	}
+	return Time(time.Date(1000, time.January, 1, 0, 0, 0, 0, time.UTC))
 }
 
 func MysqlMax() Time {
-	return Time{
-		Time: time.Date(9999, time.December, 31, 23, 59, 59, 999999999, time.UTC),
-	}
+	return Time(time.Date(9999, time.December, 31, 23, 59, 59, 999999999, time.UTC))
 }
 
 func (t Time) Before(u Time) bool {
-	return t.Time.Before(u.Time)
+	return time.Time(t).Before(time.Time(u))
 }
 
 func (t Time) After(u Time) bool {
-	return t.Time.After(u.Time)
+	return time.Time(t).After(time.Time(u))
 }
 
 func (t Time) Add(d time.Duration) Time {
-	return Time{Time: t.Time.Add(d)}
+	return Time(time.Time(t).Add(d))
 }
 
 func (t Time) MarshalJSON() ([]byte, error) {
-	if y := t.Year(); y < 0 || y >= 10000 {
+	if y := time.Time(t).Year(); y < 0 || y >= 10000 {
 		return nil, errors.New("Time.MarshalJSON: year outside of range [0,9999]")
 	}
 
 	b := make([]byte, 0, len(time.RFC3339)+2)
 	b = append(b, '"')
-	b = t.AppendFormat(b, time.RFC3339)
+	b = time.Time(t).AppendFormat(b, time.RFC3339)
 	b = append(b, '"')
 	return b, nil
 }
 
+func (t *Time) UnmarshalJSON(data []byte) error {
+	return (*time.Time)(t).UnmarshalJSON(data)
+}
+
 func (t Time) Value() (driver.Value, error) {
-	return t.Time, nil
+	return time.Time(t), nil
 }
