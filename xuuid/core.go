faf1651947ce2d2aa9709b8d975182aa2b878fcc
Boss Yu <cure1218@gmail.com>
2022-11-29 22:46:36 +0000

    boss

--- xuuid/core.go
+++ xuuid/core.go
@@ -3,20 +3,19 @@ package xuuid
 import (
 	"database/sql/driver"
 	"encoding/json"
-	"fmt"
 	"github.com/google/uuid"
 )
 
-//================================================================
+// ================================================================
 // UUID
-//================================================================
+// ================================================================
 type UUID uuid.UUID
 
 func New() UUID {
 	return UUID(uuid.New())
 }
 
-func (xuuid *UUID) UnmarshalJSON(data []byte) error {
+func (xu *UUID) UnmarshalJSON(data []byte) error {
 	var s string
 	if err := json.Unmarshal(data[:], &s); err != nil {
 		return err
@@ -25,197 +24,131 @@ func (xuuid *UUID) UnmarshalJSON(data []byte) error {
 	} else if u, err := uuid.Parse(s); err != nil {
 		return err
 	} else {
-		*xuuid = UUID(u)
+		*xu = UUID(u)
 		return nil
 	}
 }
 
-func (xuuid *UUID) Scan(src interface{}) error {
-	return (*uuid.UUID)(xuuid).Scan(src)
+func (xu *UUID) UnmarshalBinary(data []byte) error {
+	return (*uuid.UUID)(xu).UnmarshalBinary(data)
 }
 
-func (xuuid UUID) Value() (driver.Value, error) {
-	return xuuid.MarshalBinary()
+func (xu UUID) MarshalBinary() ([]byte, error) {
+	return uuid.UUID(xu).MarshalBinary()
 }
 
-func (xuuid UUID) MarshalBinary() ([]byte, error) {
-	return uuid.UUID(xuuid).MarshalBinary()
+func (xu *UUID) UnmarshalText(data []byte) error {
+	return (*uuid.UUID)(xu).UnmarshalText(data)
 }
 
-func (xuuid *UUID) UnmarshalBinary(data []byte) error {
-	return (*uuid.UUID)(xuuid).UnmarshalBinary(data)
+func (xu UUID) MarshalText() ([]byte, error) {
+	return uuid.UUID(xu).MarshalText()
 }
 
-func (xuuid *UUID) UnmarshalText(data []byte) error {
-	return (*uuid.UUID)(xuuid).UnmarshalText(data)
+func (xu UUID) String() string {
+	return uuid.UUID(xu).String()
 }
 
-func (xuuid UUID) MarshalText() ([]byte, error) {
-	return uuid.UUID(xuuid).MarshalText()
+func (xu *UUID) Scan(src interface{}) error {
+	return (*uuid.UUID)(xu).Scan(src)
 }
 
-func (xuuid UUID) String() string {
-	return uuid.UUID(xuuid).String()
+func (xu UUID) Value() (driver.Value, error) {
+	return xu.MarshalBinary()
 }
 
-//================================================================
+// ================================================================
 // Wildcard
-//================================================================
-type WildcardType uint8
-
-const (
-	WildcardTypeUndef WildcardType = iota
-	WildcardTypeString
-	WildcardTypeXUUID
-)
-
-type Wildcard struct {
-	Type WildcardType
-	Val  interface{}
-}
+// ================================================================
+type Wildcard []byte
 
 func (w *Wildcard) UnmarshalJSON(data []byte) error {
-	w.Type = WildcardTypeUndef
-	if len(data) <= 0 {
+	u, err := uuid.Parse(string(data))
+	if err != nil {
+		// string
+		*w = data
 		return nil
 	}
 
-	var s string
-	if err := json.Unmarshal(data[:], &s); err != nil {
+	if b, err := u.MarshalBinary(); err != nil {
 		return err
-	}
-
-	if u, err := uuid.Parse(s); err == nil {
-		w.Type = WildcardTypeXUUID
-		w.Val = UUID(u)
 	} else {
-		w.Type = WildcardTypeString
-		w.Val = s
-	}
-
-	return nil
-}
-
-func (w *Wildcard) Scan(src interface{}) error {
-	w.Type = WildcardTypeUndef
-	switch src := src.(type) {
-	case nil:
+		// uuid
+		*w = b
 		return nil
-
-	case string:
-		if src == "" {
-			w.Type = WildcardTypeString
-			return nil
-		}
-
-		if u, err := uuid.Parse(src); err == nil {
-			w.Type = WildcardTypeXUUID
-			w.Val = UUID(u)
-		} else {
-			w.Type = WildcardTypeString
-			w.Val = src
-		}
-
-	case []byte:
-		if len(src) == 0 {
-			return nil
-		}
-
-		if len(src) != 16 {
-			return w.Scan(string(src))
-		}
-
-		w.Type = WildcardTypeXUUID
-		u := uuid.UUID{}
-		copy((u)[:], src)
-		w.Val = UUID(u)
-
-	default:
-		return fmt.Errorf("Wildcard: unable to scan type %T", src)
-	}
-
-	return nil
-}
-
-func (w Wildcard) Value() (driver.Value, error) {
-	switch w.Type {
-	case WildcardTypeString:
-		return w.Val.(string), nil
-	case WildcardTypeXUUID:
-		return w.Val.(UUID).Value()
-	default:
-		return nil, fmt.Errorf("Wildcard: Invalid value %v.", w.Val)
-	}
-}
-
-func (w Wildcard) MarshalBinary() ([]byte, error) {
-	switch w.Type {
-	case WildcardTypeString:
-		return json.Marshal(w.Val.(string))
-	case WildcardTypeXUUID:
-		return w.Val.(UUID).MarshalBinary()
-	default:
-		return nil, fmt.Errorf("Wildcard: Failed to MarshalBinary on %v.", w.Val)
 	}
 }
 
 func (w *Wildcard) UnmarshalBinary(data []byte) error {
-	if len(data) <= 0 {
+	u, err := uuid.FromBytes(data)
+	if err != nil {
+		// string
+		*w = data
 		return nil
 	}
 
-	var (
-		u uuid.UUID
-		s string
-	)
-
-	if err := u.UnmarshalBinary(data); err == nil {
-		w.Type = WildcardTypeXUUID
-		w.Val = UUID(u)
+	if b, err := u.MarshalBinary(); err != nil {
+		return err
+	} else {
+		// uuid
+		*w = b
 		return nil
 	}
+}
 
-	if err := json.Unmarshal(data, &s); err == nil {
-		w.Type = WildcardTypeString
-		w.Val = s
-		return nil
+func (w Wildcard) MarshalBinary() ([]byte, error) {
+	if u, err := uuid.FromBytes(w); err != nil {
+		// string
+		return w, nil
+	} else {
+		// uuid
+		return u.MarshalBinary()
 	}
-
-	return fmt.Errorf("Wildcard: Failed to UnmarshalBinary.")
 }
 
 func (w *Wildcard) UnmarshalText(data []byte) error {
-	var u uuid.UUID
-
-	if err := u.UnmarshalText(data); err == nil {
-		w.Type = WildcardTypeXUUID
-		w.Val = UUID(u)
-	} else {
-		w.Type = WildcardTypeString
-		w.Val = string(data[:])
+	u, err := uuid.Parse(string(data))
+	if err != nil {
+		// string
+		*w = data
+		return nil
 	}
 
-	return nil
+	if b, err := u.MarshalBinary(); err != nil {
+		return err
+	} else {
+		// uuid
+		*w = b
+		return nil
+	}
 }
 
 func (w Wildcard) MarshalText() ([]byte, error) {
-	switch w.Type {
-	case WildcardTypeString:
-		return []byte(w.Val.(string)), nil
-	case WildcardTypeXUUID:
-		return w.Val.(UUID).MarshalText()
-	default:
-		return nil, fmt.Errorf("Wildcard: Failed to MarshalText on %v.", w.Val)
+	if u, err := uuid.FromBytes(w); err != nil {
+		// string
+		return w, nil
+	} else {
+		// uuid
+		return u.MarshalText()
 	}
 }
 
 func (w Wildcard) String() string {
-	switch w.Type {
-	case WildcardTypeString:
-		return w.Val.(string)
-	case WildcardTypeXUUID:
-		return w.Val.(UUID).String()
-	default:
-		return ""
+	if u, err := uuid.FromBytes(w); err != nil {
+		// string
+		return string(w)
+	} else {
+		// uuid
+		return u.String()
+	}
+}
+
+func (w Wildcard) Value() (driver.Value, error) {
+	if u, err := uuid.FromBytes(w); err != nil {
+		// string
+		return string(w), nil
+	} else {
+		// uuid
+		return UUID(u).Value()
 	}
 }
